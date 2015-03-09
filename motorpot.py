819384851f54fd2727cbcc452fe7dd883145467b
Scott Baker <smbaker@gmail.com>
2015-03-08 22:32:12 -0700

    threaded it

--- motorpot.py
+++ motorpot.py
@@ -1,10 +1,13 @@
 import sys
 import time
+from threading import Thread
 from motor import Motor, L293_1, L293_2, L293_ENABLE
 from ads1015 import ADS1015, MUX_AIN0, PGA_4V, MODE_CONT, DATA_1600, COMP_MODE_TRAD, COMP_POL_LOW, COMP_NON_LAT, COMP_QUE_DISABLE
 
-class MotorPot:
-    def __init__(self, bus, adc_addr=0x48, motor_pin1=L293_1, motor_pin2=L293_2, motor_enable = L293_ENABLE, dirmult=1):
+class MotorPot(Thread):
+    def __init__(self, bus, adc_addr=0x48, motor_pin1=L293_1, motor_pin2=L293_2, motor_enable = L293_ENABLE, dirmult=1, verbose=False):
+        Thread.__init__(self)
+
         self.motor = Motor(pin1=motor_pin1, pin2=motor_pin2, enable = motor_enable)
         self.motor.set_speed(0)
 
@@ -14,40 +17,67 @@ class MotorPot:
 
         self.dirmult = dirmult
 
+        self.setPoint = None
+        self.newSetPoint = False
+
+        self.daemon = True
+        self.verbose = verbose
+
+        self.start()
+
     def set(self, value):
-        settle = 0
+        self.setPoint = value
+        self.newSetPoint = True
+
+    def run(self):
+        setPoint = None
         while True:
-            v = self.adc.read_conversion()
-            if (v < value):
-                dir = 1
-            else:
-                dir = -1
+            self.value = self.adc.read_conversion()
 
-            # the 'P' part of a PID controller...
-            error = abs(v - value)
-            if (error <= 1):
-                speed = 0
-
-                # are we done yet?
-                settle=settle+1
-                if (settle>10):
-                    return
-            else:
+            if self.newSetPoint:
+                setPoint = self.setPoint
+                self.newSetPoint=False
                 settle = 0
-                if (error < 10):
-                    settle = 0
-                    speed = 50
-                elif (error < 100):
-                    speed = 75
+
+            if setPoint is not None:
+                if (self.value < setPoint):
+                    dir = 1
                 else:
-                    speed = 100
+                    dir = -1
 
-            print v, value, dir, speed
+                # the 'P' part of a PID controller...
+                error = abs(self.value - setPoint)
+                if (error <= 1):
+                    speed = 0
 
-            self.motor.set_dir(dir * self.dirmult)
-            self.motor.set_speed(speed)
+                    # are we done yet?
+                    settle=settle+1
+                    if (settle>10):
+                        setPoint = None
+                else:
+                    settle = 0
+                    if (error < 10):
+                        settle = 0
+                        speed = 50
+                    elif (error < 100):
+                        speed = 75
+                    else:
+                        speed = 100
 
-            time.sleep(0.001)
+                if self.verbose:
+                    print "moving", self.value, setPoint, dir, speed
+
+                self.motor.set_dir(dir * self.dirmult)
+                self.motor.set_speed(speed)
+
+            if setPoint is not None:
+                # fine-grained timing while we're moving the pot
+                time.sleep(0.001)
+            else:
+                if self.verbose:
+                    print "monitor", self.value
+                # course-grained if we're just reading it
+                time.sleep(0.1)
 
 def main():
     if len(sys.argv)<2:
@@ -59,14 +89,13 @@ def main():
     import smbus
     bus = smbus.SMBus(1)
 
-    motorpot = MotorPot(bus, dirmult=-1)
+    motorpot = MotorPot(bus, dirmult=-1, verbose=True)
 
-    if sys.argv[1]=="monitor":
-        while True:
-            print motorpot.adc.read_conversion()
-            time.sleep(0.1)
-    else:
+    if sys.argv[1]!="none":
         motorpot.set(int(sys.argv[1]))
+
+    while True:
+        time.sleep(1)
 
 
 if __name__== "__main__":
