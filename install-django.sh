eba52afb90c31c663bc3710d5e1f1b965c1a1796
Scott Baker <smbaker@gmail.com>
2015-03-15 00:59:03 -0700

    more setup scripts

---
+++ install-django.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install python-pip
+sudo apt-get -y install sqlite3
+sudo apt-get -y install python-dev
+sudo pip install Django==1.7
+sudo pip install pyephem
---
+++ setup-pianobar.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+#sudo apt-get -y install json-devel gcrypt-devel gnutls-devel
+#sudo apt-get -y install gmake pthreads labao gnutls gcrypt libfaad2 libmad UTF-8
+sudo apt-get -y install libevent-pthreads-2.0-5 libao-dev libgnutls-dev libmad0-dev libfaad-dev libjson0-dev
