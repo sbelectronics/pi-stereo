76290582d8014a31ce4ac60c9c032579ad9b77f5
Scott Baker <smbaker@gmail.com>
2015-03-15 16:06:36 -0700

    save now playing to a file

---
+++ dotconfig/pianobar/eventcmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# create variables
+while read L; do
+    k="`echo "$L" | cut -d '=' -f 1`"
+    v="`echo "$L" | cut -d '=' -f 2`"
+    export "$k=$v"
+done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
+
+case "$1" in
+        songstart)
+                 rm /home/pi/.config/pianobar/scripts/out
+                 echo -e "$title\n$artist\n$stationName" >> /home/pi/.config/pianobar/now_playing
+esac
--- setup-pianobar.sh
+++ setup-pianobar.sh
@@ -5,3 +5,11 @@ sudo apt-get -y install libevent-pthreads-2.0-5 libao-dev libgnutls-dev libmad0-
 
 sudo emacs /etc/modprobe.d/alsa-base.conf
   # comment out "options snd-usb-audio index=-2"
+
+# copy pianobar config file from dotconfig/pianobar/config to .config/pianobar/config
+
+emacs .config/pianobar/config
+    event_command = /home/pi/.config/pianobar/eventcmd.sh
+    fifo = /home/pi/.config/pianobar/ctl
+
+mkfifo /home/pi/.config/pianobar/ctl
