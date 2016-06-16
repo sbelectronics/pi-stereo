7d71ad7100513043cd56c2abc6c013e9c2f88b78
Scott Baker <smbaker@gmail.com>
2016-06-15 21:21:01 -0700

    exclude royalty_free and git dirs

--- up.sh
+++ up.sh
@@ -1,1 +1,1 @@
-rsync -avz --exclude "__history" --exclude "*~" --exclude "*.img" --exclude "home-pi-*" --exclude "dot*" --exclude "*.conf" --exclude "*.pyc" -e ssh . pi@198.0.0.215:/home/pi/stereo
+rsync -avz --exclude "__history" --exclude "*~" --exclude "*.img" --exclude "home-pi-*" --exclude "dot*" --exclude "*.conf" --exclude "*.pyc" --exclude "royalty_free" --exclude ".git" -e ssh . pi@198.0.0.215:/home/pi/stereo
