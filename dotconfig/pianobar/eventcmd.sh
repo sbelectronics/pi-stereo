ee25f8957e233f29de464d4355568049807f3f49
Scott Baker <smbaker@gmail.com>
2016-06-15 21:19:28 -0700

    tmpfs for pianobar state

--- dotconfig/pianobar/eventcmd.sh
+++ dotconfig/pianobar/eventcmd.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # create variables
-rm -f /home/pi/.config/pianobar/now_playing_vars.tmp
+rm -f /var/pianobar/now_playing_vars.tmp
 while read L; do
     k="`echo "$L" | cut -d '=' -f 1`"
     v="`echo "$L" | cut -d '=' -f 2`"
     export "$k=$v"
-    echo "$k=$v" >> /home/pi/.config/pianobar/now_playing_vars.tmp
+    echo "$k=$v" >> /var/pianobar/now_playing_vars.tmp
 done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
 
-mv /home/pi/.config/pianobar/now_playing_vars.tmp /home/pi/.config/pianobar/now_playing_vars
+mv /var/pianobar/now_playing_vars.tmp /var/pianobar/now_playing_vars
 
 case "$1" in
         songstart)
-                 echo -e "$title\n$artist\n$stationName" > /home/pi/.config/pianobar/now_playing
+                 echo -e "$title\n$artist\n$stationName" > /var/pianobar/now_playing
 esac
