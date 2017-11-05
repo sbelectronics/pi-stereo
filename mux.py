aa4812b39213b8899901efac4de740b87fcb17d3
Scott Baker <smbaker@gmail.com>
2017-11-04 22:00:22 -0700

    support for my new muxer board

---
+++ mux.py
@@ -0,0 +1,26 @@
+import sys
+from ioexpand import PCF8574
+
+MUX_ADDR = 0x20
+
+class Mux(PCF8574):
+    def __init__(self, bus, addr):
+        PCF8574.__init__(self, bus, addr)
+        self.bus.write_byte(self.addr, 0x00)  # all IO are outputs
+
+    def set_channel(self, i):
+        self.set_gpio(0, i)
+
+def main():
+    import smbus
+
+    if len(sys.argv)<2:
+        print "syntax: mux <channel-num>"
+        sys.exit(-1)
+
+    bus = smbus.SMBus(1)    
+    mux = Mux(bus, MUX_ADDR)
+    mux.set_channel(int(sys.argv[1]))
+
+if __name__ == "__main__":
+    main()
